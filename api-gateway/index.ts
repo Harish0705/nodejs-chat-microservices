f29adb7fe8ca04c2ec5dc2f4657bf0b6d88cb2c7
Harish0705 <harish.balachandar@nzme.co.nz>
2024-09-28 17:48:44 +1200

    update api-gateway

--- api-gateway/index.ts
+++ api-gateway/index.ts
@@ -1,20 +1,24 @@
 import express from "express";
 import proxy from "express-http-proxy";
+import "dotenv/config";
+import "express-async-errors";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const auth = proxy("http://localhost:8081");
-const messages = proxy("http://localhost:8082");
-const notifications = proxy("http://localhost:8083");
+const auth = proxy("http://localhost:4000");
+const messages = proxy("http://localhost:3000");
+const notifications = proxy("http://localhost:7000");
 
 app.use("/api/v1/auth", auth);
 app.use("/api/v1/message", messages);
 app.use("/api/notifications", notifications);
 
-const server = app.listen(8080, () => {
-    console.log("Gateway is Listening to Port 8080");
+const port = process.env.PORT || 5000;
+
+const server = app.listen(port, () => {
+    console.log(`Gateway is Listening to Port ${port}`);
 });
 
 const exitHandler = () => {
